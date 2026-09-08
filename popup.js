7f2ae3ddba5807f5969401d419539ae5c600e536
Nashwan Beiruti <nashwan.beiruti1@gmail.com>
2026-09-07 21:36:21 -0400

    Delete popup.js

--- popup.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('openMaps').addEventListener('click', () => {
-  const address = document.getElementById('address').value;
-  if (address) {
-    chrome.runtime.sendMessage({ action: 'openInGoogleMaps', address: address });
-  } else {
-    alert('Please enter an address.');
-  }
-});
